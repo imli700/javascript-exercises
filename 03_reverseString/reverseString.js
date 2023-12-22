fae90480f047c222eb8f2d241a09a5cff52ae26f
imli700 <imlijangba@gmail.com>
2023-12-22 18:20:47 +0530

    Finish exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(toReverse) {
+  const brokenDown = toReverse.split('');
+  let lastIndex = brokenDown.length - 1;
+  let reversedString = '';
 
+  for (; lastIndex >= 0; lastIndex--) {
+    reversedString += toReverse[lastIndex];
+  }
+  return reversedString;
 };
 
 // Do not edit below this line
