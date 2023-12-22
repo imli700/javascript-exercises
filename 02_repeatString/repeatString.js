336ce532f30d7e6ca9d41315c39b5f5ce3e9cccb
imli700 <imlijangba@gmail.com>
2023-12-22 18:08:26 +0530

    Finish exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str, repeat) {
+  if (repeat < 0) {
+    return 'ERROR';
+  } else if (repeat === 0) {
+    return '';
+  } else {
+    let output = '';
+    for (let i = 0; i < repeat; i++) {
+      output += str;
+    }
+    return output;
+  }
 };
 
 // Do not edit below this line
