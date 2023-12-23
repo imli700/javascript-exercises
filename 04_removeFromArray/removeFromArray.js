44e3e9ab6a3808b312a59f9ce86d48c9b94168b8
imli700 <imlijangba@gmail.com>
2023-12-23 17:24:08 +0530

    Finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(originalArray, ...toRemove) {
+  let intervalsLeft = originalArray.length;
 
+  for (let i = 0; i < toRemove.length; i++) {
+    for (let j = 0; j < intervalsLeft; j++) {
+      if (toRemove[i] === originalArray[j]) {
+        originalArray.splice(j, 1);
+        intervalsLeft--;
+        break;
+      }
+    }
+  }
+
+  return originalArray;
 };
 
 // Do not edit below this line
