b4eed1a308e855bf10d57c5612a8dec2a4899942
imli700 <imlijangba@gmail.com>
2023-12-23 17:53:04 +0530

    Finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if (typeof (num1) != "number" || typeof (num2) != "number" || num1 < 0 || num2 < 0) {
+    return "ERROR";
+  }
 
+  if (num1 > num2) {
+    let temp = num1;
+    num1 = num2;
+    num2 = temp;
+  }
+
+  let sum = num1
+
+  for (let i = num1 + 1; i <= num2; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
