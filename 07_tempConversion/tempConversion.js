5055cc91f4720b1b5dc24db1356b1a04ed31838f
imli700 <imlijangba@gmail.com>
2023-12-24 09:53:29 +0530

    Finish exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,10 @@
-const convertToCelsius = function() {
-};
-
-const convertToFahrenheit = function() {
+const convertToCelsius = function(fahrenheit) {
+  let result = Number((5 / 9 * (fahrenheit - 32)).toFixed(1));
+  return result;
+}
+const convertToFahrenheit = function(celsius) {
+  let result = Number((9 / 5 * celsius + 32).toFixed((1)))
+  return result;
 };
 
 // Do not edit below this line
