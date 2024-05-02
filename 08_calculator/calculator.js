aba8bc244598c18f14526812316ba237c66a5670
imli700 <imlijangba@gmail.com>
2024-05-02 18:11:35 +0530

    Finish exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr.length) {
+
+    let value = arr.reduce((accu, currentValue) => {
+      return accu + currentValue;
+    }, 0)
+
+    return value;
+  } else return 0;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let result = arr.reduce((accu, current) => {
+    return accu * current;
+  })
 
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let fact = number;
+
+  if (number === 0) {
+    return 1;
+  }
+
+  for (let i = 1; i < number; i++) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
