9926f73f91d03e2b79bfe697de68c6a5020a3a69
imli700 <imlijangba@gmail.com>
2024-05-03 10:26:23 +0530

    Finish exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+  let titles = [];
 
+  titles = books.map((value) => {
+    return value.title;
+  })
+  return titles;
 };
 
 // Do not edit below this line
