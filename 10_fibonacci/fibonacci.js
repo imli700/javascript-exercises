c6f41076849a62a546ccbb22af1a8dab01c7715b
imli700 <imlijangba@gmail.com>
2024-05-03 10:08:56 +0530

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
-};
+function fibonacci(n) {
+  if (n <= 0) {
+    return 0; // Since Fibonacci sequence index you're using starts at 1
+  }
+  let a = 1, b = 1;
+  for (let i = 3; i <= n; i++) {
+    let c = a + b;
+    a = b;
+    b = c;
+  }
+  return b;
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
